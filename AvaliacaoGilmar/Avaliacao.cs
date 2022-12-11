280eb25da745ee6243c414d474f6c0e352675785
Rafael Estanislau <rafalevel100@hotmail.com>
2022-12-11 15:25:45 -0300

    caso base de teste unitario adicionado

---
+++ AvaliacaoGilmar/Avaliacao.cs
@@ -0,0 +1,34 @@
+﻿namespace AvaliacaoGilmar;
+public class Avaliacao
+{
+
+    public string Avalia(double nota1, double nota2, double faltas, double cargaHoraria)
+    {
+        string result;
+        double percentualFaltas = (faltas / cargaHoraria);
+        double media = (nota1 + nota2) / 2;
+        if ((nota1 < 0.0 || nota1 > 10) ||
+                (nota2 < 0.0 || nota2 > 10) ||
+                (faltas < 0.0 || faltas > cargaHoraria) ||
+                cargaHoraria < 0.0){
+            throw new ValoresInvalidosException();//result = "Valores Inválidos.";
+        }else if (percentualFaltas > 0.25)
+        {
+            result = "Reprovado por Falta.";
+        }
+        else if (media < 3.0)
+        {
+            result = "Reprovado por Média.";
+        }
+        else if (media >= 3.0 && media < 6.0)
+        {
+            result = "Prova Extra.";
+        }
+        else
+        {
+            result = "Aprovado.";
+        }
+        return result;
+    }
+
+}
---
+++ AvaliacaoTeste/AvaliacaoTesteUnitario.cs
@@ -0,0 +1,24 @@
+﻿using CalculoNota;
+
+namespace AvaliacaoTeste
+{
+    public class AvaliacaoTesteUnitario
+    {
+        [Fact]
+        public void NotaAbaixoDeTresResultaEmReprovacaoPorMedia()
+        {
+            // Arrange  
+            var nota1 = 2.9;
+            var nota2 = 3.0;
+            var faltas = 6;
+            var cargaHoraria = 48;
+            var resultadoEsperado = "Reprovado por Média.";
+            // Act  
+
+            var resultado = Avaliacao.Avalia(nota1, nota2, faltas, cargaHoraria);
+
+            //Assert  
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+    }
+}
